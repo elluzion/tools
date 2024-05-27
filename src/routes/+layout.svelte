a7d98f1111376e7eaf096e393629d9f4209624ba
Fynn <fynnruebenach@gmail.com>
2024-05-27 22:05:07 +0200

    💄 Add basic theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import '../app.css';
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    @apply font-mono bg-background text-text-main;
+  }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Geist"', '"Roboto"', 'sans-serif'],
+        mono: ['"JetBrains Mono"', 'monospace'],
+      },
+      colors: {
+        'text-main': 'var(--color-text-main)',
+        'text-muted': 'var(--color-text-muted)',
+        background: 'var(--color-background)',
+        'elevation-1': 'var(--color-elevation-1)',
+        'elevation-2': 'var(--color-elevation-2)',
+        'elevation-3': 'var(--color-elevation-3)',
+        'primary-action': 'var(--color-primary-action)',
+        'on-primary-action': 'var(--color-on-primary-action)',
+        'secondary-action': 'var(--color-secondary-action)',
+        'on-secondary-action': 'var(--color-on-secondary-action)',
+        warning: 'var(--color-warning)',
+        'on-warning': 'var(--color-on-warning)',
+        border: 'var(--color-border)',
+        ring: 'var(--color-ring)',
+      },
+    },
   },
   plugins: [],
 };
