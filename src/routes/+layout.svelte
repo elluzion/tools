f63e0634839ca18e1f8eb5a6fe967a3c4b436a68
Fynn <fynnruebenach@gmail.com>
2024-05-29 22:53:20 +0200

    ✨ Add loading spinner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script>
+  import { navigating } from '$app/stores';
   import { PageHeader } from '$lib/components/page-header';
+  import { Circle } from 'svelte-loading-spinners';
   import '../app.css';
 </script>
 
 <div>
   <PageHeader />
-  <div class="fixed flex justify-center w-full pt-16">
+  {#if $navigating}
+    <div class="fixed z-50 flex items-center justify-center w-full h-full">
+      <Circle size="32" color="#fafafa" unit="px" duration="1s" />
+    </div>
+  {/if}
+  <div
+    style="opacity: {$navigating ? 0 : 1};
+      pointer-events: {$navigating ? 'none' : 'all'};
+      scale: {$navigating ? 0.99 : 1};
+      "
+    class="flex justify-center w-screen min-h-screen pt-16 overflow-x-hidden overflow-y-auto transition-all ease-in-out"
+  >
     <slot />
   </div>
 </div>
