17746d73f24b2e4e8e9962bf590772ff6cce620d
Fynn <fynnruebenach@gmail.com>
2024-06-01 19:04:38 +0200

    🔧 Add tailwind contentDvh spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,9 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      spacing: {
+        contentDvh: 'calc(100dvh - 64px)',
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
