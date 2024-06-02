8b94d28faf6acf02be0806418ef5c50dad025dcc
Fynn <fynnruebenach@gmail.com>
2024-06-02 19:26:11 +0200

    ♻️ Move Card into own component

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,6 @@
+<div
+  {...$$restProps}
+  class="relative w-full p-5 overflow-hidden bg-card rounded-3xl {$$restProps.class || ''}"
+>
+  <slot />
+</div>
--- src/routes/_components/latest-release-card.svelte
+++ src/routes/_components/latest-release-card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
+  import Card from '$lib/components/card.svelte';
   import Badge from '$lib/components/ui/badge/badge.svelte';
   import Button from '$lib/components/ui/button/button.svelte';
   import type { Song } from '$lib/types/song';
@@ -8,9 +9,7 @@
   export let song: Song;
 </script>
 
-<div
-  class="relative flex flex-col w-full min-h-[200px] overflow-hidden gap-4 p-5 bg-card rounded-3xl"
->
+<Card class="flex flex-col gap-4">
   <div class="gap-2 w-[calc(100%-72px)]">
     <h5 class="text-xl font-semibold truncate">{song.title}</h5>
     <p class="font-medium truncate text-muted-text">{Formatter.joinList(song.artists)}</p>
@@ -32,4 +31,4 @@
     alt="Song cover"
     class="absolute w-16 h-16 rounded-lg select-none right-5"
   />
-</div>
+</Card>
