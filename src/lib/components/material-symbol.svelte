8267f726e4ffa459701669f6436a308ff3e9fb2c
Fynn <fynnruebenach@gmail.com>
2024-06-02 21:05:02 +0200

    🎨 MaterialSymbol: Forward props

--- src/lib/components/material-symbol.svelte
+++ src/lib/components/material-symbol.svelte
@@ -2,7 +2,7 @@
   /** Material Symbols Reference: https://fonts.google.com/icons?selected=Material+Symbols+Outlined */
 </script>
 
-<span class="material-symbols-outlined"> <slot /> </span>
+<span {...$$restProps} class={`${$$restProps.class} material-symbols-outlined`}> <slot /> </span>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0');
