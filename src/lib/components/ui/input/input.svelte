85482c682ff94a43a8ed69a49b2305ef3641872c
Fynn <fynnruebenach@gmail.com>
2024-06-11 17:09:50 +0200

    💄 Increase shadcn input height

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -17,7 +17,7 @@
 
 <input
   class={cn(
-    'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+    'flex h-12 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
     className,
   )}
   bind:value
@@ -40,3 +40,9 @@
   on:wheel|passive
   {...$$restProps}
 />
+
+<style scoped>
+  input[type='date']::-webkit-calendar-picker-indicator {
+    display: none;
+  }
+</style>
