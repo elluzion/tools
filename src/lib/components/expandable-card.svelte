3a6f44280c1e5c7c6bd4d27aede25bddb239660c
Fynn <fynnruebenach@gmail.com>
2024-06-20 01:28:13 +0200

    🎨 ExpandableCard: Stop using magic numbers for collapsed and expanded height

--- src/lib/components/expandable-card.svelte
+++ src/lib/components/expandable-card.svelte
@@ -6,20 +6,29 @@
   export let expanded = true;
   export let title = 'Title';
 
-  const collapsedHeight = 64;
+  const previewHeight = 24;
+  const padding = 24;
+  const cardGap = 12;
+  const collapsedHeight = previewHeight + padding * 2;
   let content: HTMLDivElement;
 
-  $: expandedHeight = 64 + 16 + content?.offsetHeight ?? 0;
+  $: expandedHeight = collapsedHeight + cardGap + content?.offsetHeight ?? 0;
 </script>
 
 <Card
   {...$$restProps}
-  class="flex flex-col gap-4 transition-[height] duration-200 {$$restProps.class}"
-  style="height: {expanded ? expandedHeight : collapsedHeight}px;{$$restProps.style}"
+  class="flex flex-col transition-[height] duration-200 {$$restProps.class}"
+  style="height: {expanded
+    ? expandedHeight
+    : collapsedHeight}px; padding: {padding}px; gap: {cardGap}px;{$$restProps.style}"
 >
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <div class="flex h-6 cursor-pointer" on:click={() => (expanded = !expanded)}>
+  <div
+    class="flex cursor-pointer"
+    on:click={() => (expanded = !expanded)}
+    style="height: {previewHeight}px;"
+  >
     <p class="font-bold grow">{title}</p>
     <Button
       class="relative flex justify-end h-6 px-0 transition-transform rounded-full hover:bg-elevation-1"
