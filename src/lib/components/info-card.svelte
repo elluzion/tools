763bec560245a0744a30e08d92b56953ea1735e5
Fynn <fynnruebenach@gmail.com>
2024-07-13 20:35:25 +0200

    💄 Add InfoCard

---
+++ src/lib/components/info-card.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Card from './card.svelte';
+  import MaterialSymbol from './material-symbol.svelte';
+</script>
+
+<Card class="flex !p-5 gap-5 *:text-muted-text">
+  <MaterialSymbol>info</MaterialSymbol>
+  <p class="font-mono">
+    <slot />
+  </p>
+</Card>
