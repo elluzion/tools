65ce4c1b08274b54d279680e77846aa9d39458e0
Fynn <fynnruebenach@gmail.com>
2024-07-30 20:36:35 +0200

    🧱 Stop handling API routes in sveltekit

--- src/routes/[...slugs]/+server.ts
+++
@@ -1,6 +0,0 @@
-import api from '$api';
-
-type RequestHandler = (v: { request: Request }) => Response | Promise<Response>;
-
-export const GET: RequestHandler = ({ request }) => api.handle(request);
-export const POST: RequestHandler = ({ request }) => api.handle(request);
