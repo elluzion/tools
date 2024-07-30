274791954c80c668c91dcc014287545c36b7ff38
Fynn <fynnruebenach@gmail.com>
2024-07-30 21:57:45 +0200

    👽️ Remove API /api subpath

--- src/lib/utilities/api.ts
+++ src/lib/utilities/api.ts
@@ -2,4 +2,4 @@ import type { Api as ApiType } from '$api';
 import { PUBLIC_TOOLS_API_URL as API_URL } from '$env/static/public';
 import { treaty } from '@elysiajs/eden';
 
-export const Api = treaty<ApiType>(API_URL).api;
+export const Api = treaty<ApiType>(API_URL);
